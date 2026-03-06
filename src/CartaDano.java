64ac87fd4142365cead326bc032ad6289cb7a42e
enzofarinamullis <e243589@dac.unicamp.br>
2026-03-06 17:29:48 -0300

    Criar base para as cartas

--- src/CartaDano.java
+++ src/CartaDano.java
@@ -10,20 +10,16 @@ class Carta_de_dano{
 public class CartaDano{
 >>>>>>> refs/remotes/origin/main
   public String nome;
-  public int custo_de_energia;
-  public int forca;
-  //public String efeito;
+  public int custoEnergia;
+  public int dano;
 
-  public CartaDano(String nome, int custo_de_energia) {
+  public CartaDano(String nome, int custoEnergia) {
     this.nome = nome;
-    this.custo_de_energia = custo_de_energia;
-    this.forca = forca;
-    this.efeito = efeito;
+    this.custoEnergia = custoEnergia;
   }
 
-  public void usar_a_carta(String nome, int custo_de_energia, int for
-
-
+  public void usar(Inimigo inimigo){
+    inimigo.receberDano(this.dano);
   }
 
 
