28c39770629f826cebe9792e1a7f56fa84902aa3
ra288660 <a288660@dac.unicamp.com>
2026-03-06 17:21:33 -0300

    commit 2

--- src/CartaDano.java
+++ src/CartaDano.java
@@ -1,13 +1,24 @@
+// dinâmica de ataque:
+// 1-Declara o ataque
+// 2-roda d20 + bonus
+// 3-se romper o escudo é aplicado o efeito
+//obs: só pode um ataque por turno
+
 class Carta_de_dano{
   public String nome;
-  int custo_de_energia;
+  public int custo_de_energia;
+  public int forca;
+  //public String efeito;
 
   public Carta_de_dano(String nome, int custo_de_energia) {
     this.nome = nome;
     this.custo_de_energia = custo_de_energia;
+    this.forca = forca;
+    this.efeito = efeito;
   }
 
-  void usar_a_carta() {
+  public void usar_a_carta(String nome, int custo_de_energia, int for
+
 
   }
 
