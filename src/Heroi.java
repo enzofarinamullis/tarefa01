a401534b0ec02284c48e15b2f7423e64238baebb
enzofarinamullis <e243589@dac.unicamp.br>
2026-03-06 16:01:51 -0300

    Adicionar classes vazias

--- src/Heroi.java
+++ src/Heroi.java
@@ -1,7 +1,31 @@
 public class Heroi{
+  public String nome;
   public int vida;
+  public int escudo;
+  public int dano;
 
-  public Heroi(int vida){
+  /* Construtor */
+  public Heroi(String nome, int vida, int escudo, int dano){
+    this.nome = nome;
     this.vida = vida;
+    this.escudo = escudo;
+    this.dano = dano;
+  }
+  
+  public void recebeDano(int dano){
+    this.vida = this.vida - dano;
+  }
+
+  public void ganharEscudo(int escudo){
+    this.escudo = this.escudo + escudo;
+  }
+
+  public int estaVivo(){
+    if(this.vida == 0){
+      return 0;
+    }
+    else{
+      return 1;
+    }
   }
 }
