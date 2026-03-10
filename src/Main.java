98d0e97acb88339c1d620cf16bdd1a73402f5141
enzofarinamullis <e243589@dac.unicamp.br>
2026-03-10 13:02:57 -0300

    Implementar Historia Inicial

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,15 @@ public class Main {
     /* so para efeitos de teste */
     cena.atualizaCena(); // amanha irei implementar a sistema de FramesPerSeconds para que o 
                          // nosso jogo tenha atualizacoes constantes
-    cena = new CenaSlime(dados);
+    //cena = new CenaSlime(dados);
+    //cena.carregaCena();
+    //cena.atualizaCena();
+
+    cena = new HistoriaInicial(dados);
     cena.carregaCena();
     cena.atualizaCena();
+
+     
   }
 
   /* Explicarei melhor tudo pessoalmente, mas basicamente, nao faremos a "jogatina" na main */
