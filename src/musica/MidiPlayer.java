df65391bb6e80cbe5c2e8c668b7e7ccd596374af
enzofarinamullis <e243589@dac.unicamp.br>
2026-03-16 15:36:31 -0300

    Referencias para o implementar musica

--- src/musica/MidiPlayer.java
+++ src/musica/MidiPlayer.java
@@ -3,7 +3,8 @@ package musica;
 import javax.sound.midi.*;
 import java.util.*;
 import java.io.File;
-
+/* ref: https://docs.oracle.com/en/java/javase/21/docs//api/java.desktop/javax/sound/midi/spi/MidiFileReader.html */
+/* ref: https://www.geeksforgeeks.org/java/java-midi/ */
 public abstract class MidiPlayer extends Thread {
   String caminho;
   
