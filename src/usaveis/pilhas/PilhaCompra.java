f51d68a45cafbb60ff43d4d21d8afbb96696dac0
enzofarinamullis <e243589@dac.unicamp.br>
2026-03-30 16:44:05 -0300

    Voltar itens

--- src/usaveis/pilhas/PilhaCompra.java
+++ src/usaveis/pilhas/PilhaCompra.java
@@ -14,15 +14,15 @@ public class PilhaCompra extends Pilha {
     super();
     /* criamos o deque */
     Carta carta = new espadaCurta();
-    //pilha.add(carta);
-    //carta = new espadaMedia();
-    //pilha.add(carta);
-    //carta = new espadaLonga();
-    //pilha.add(carta);
-    //carta = new espadaEL();
-    //pilha.add(carta);
-    //carta = new escudoPequeno();
-    //pilha.add(carta);
+    pilha.add(carta);
+    carta = new espadaMedia();
+    pilha.add(carta);
+    carta = new espadaLonga();
+    pilha.add(carta);
+    carta = new espadaEL();
+    pilha.add(carta);
+    carta = new escudoPequeno();
+    pilha.add(carta);
     carta = new escudoMedio();
     pilha.add(carta);
     carta = new escudoGrande();
