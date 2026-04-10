cba3b1fd29eef5c1523a641fccd4fead2ad310a6
enzofarinamullis <e243589@dac.unicamp.br>
2026-04-09 21:29:46 -0300

    Removendo arquivos não necessários no momento: Aliados, ListaAliados, Mapa

--- app/src/main/java/mapa/estruturas/Cidade.java
+++
@@ -1,9 +0,0 @@
-package mapa.estruturas;
-
-public class Cidade extends Estrutura {
-  public Cidade(int x, int y) {
-    super(x, y);
-    ehCidade = true;
-    ehDungeon = false;
-  }
-}
--- app/src/main/java/mapa/estruturas/Dungeon.java
+++
@@ -1,9 +0,0 @@
-package mapa.estruturas;
-
-public class Dungeon extends Estrutura {
-  public Dungeon(int x, int y) {
-    super(x, y);
-    ehCidade = false;
-    ehDungeon = true;
-  }
-}
