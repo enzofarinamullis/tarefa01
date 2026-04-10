e45cdf502b702741a8a5ddc9ae674df207df2ad9
enzofarinamullis <e243589@dac.unicamp.br>
2026-04-10 18:16:44 -0300

    Implementar corrupcao

---
+++ app/src/main/java/usaveis/cartas/efeitos/Corrupcao.java
@@ -0,0 +1,36 @@
+package usaveis.cartas.efeitos;
+
+import constantes.Turnos;
+import dados.Entidade;
+import dados.Heroi;
+import dados.Inimigo;
+import usaveis.cartas.Efeito;
+
+public class Corrupcao extends Efeito {
+  public Corrupcao(String nome, int duracao, int intensidade, int alcance) {
+    super(nome, duracao, intensidade, alcance, Turnos.INICIO_TURNO_JOAGADOR);
+  }
+  public void aplicar(Entidade entidade) {
+    if (entidade != null) {
+      int dano = getIntensidade()*10;
+      if (entidade instanceof Inimigo) {
+        Inimigo inimigo = (Inimigo) entidade;
+        inimigo.receberDano(dano);
+        System.out.println("☠️ " + entidade.getNome() + " sofreu corrupção! Perdeu " + dano + " HP");
+        
+      }
+      else if (entidade instanceof Heroi) {
+        Heroi heroi = (Heroi) entidade;
+        heroi.receberDano(dano);
+        System.out.println("☠️ " + entidade.getNome() + " sofreu corrupção! Perdeu " + dano + " HP");
+        
+      }
+    }
+    
+  }
+  
+  @Override
+  public boolean ehCurrupcao(){
+    return true;
+  }
+}
