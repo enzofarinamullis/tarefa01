015e987b936e3fc28ea4b281dc669c7a55b8ac40
enzofarinamullis <e243589@dac.unicamp.br>
2026-04-10 23:16:51 -0300

    Removendo arquivos desnecessários

--- app/src/main/java/constantes/IdsEstruturas.java
+++
@@ -1,8 +0,0 @@
-package constantes;
-
-public class IdsEstruturas {
-  public static final int ID_CIDADE = 101;
-  public static final int ID_DUNGEON = 102;
-  public static final int ID_GRAMA = 0;
-  
-}
