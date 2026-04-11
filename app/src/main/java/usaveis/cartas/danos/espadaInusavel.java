2dbd58f0996c836e89b3ebc7e6324ffff353f3e4
enzofarinamullis <e243589@dac.unicamp.br>
2026-04-10 23:03:10 -0300

    Adicionar Espada Inusavel

---
+++ app/src/main/java/usaveis/cartas/danos/espadaInusavel.java
@@ -0,0 +1,9 @@
+package usaveis.cartas.danos;
+
+import usaveis.cartas.CartaDano;
+
+public class espadaInusavel extends CartaDano {
+  public espadaInusavel() {
+    super("A Espada Que Não Pode Ser Usada", 1000000,1, "Uma espada que você nunca poderá usar" );
+  }
+}
